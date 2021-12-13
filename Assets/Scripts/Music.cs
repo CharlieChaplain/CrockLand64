09e306329cb2eb604b7845e00f1b74d48aa5cb32
LAPTOP-GV8SRDJC\steve <stevecallen159@gmail.com>
2021-12-13 18:20:45 -0500

    Added in rudimentary music support

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource intro;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (intro != null)
+        {
+            intro.volume = SoundManager.Instance.musicVolume;
+            source.volume = SoundManager.Instance.musicVolume;
+
+            intro.Play();
+            float introLength = intro.clip.length;
+            source.PlayDelayed(introLength);
+        } else
+        {
+            source.volume = SoundManager.Instance.musicVolume;
+            source.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(intro != null)
+        {
+            intro.volume = SoundManager.Instance.musicVolume;
+        }
+        source.volume = SoundManager.Instance.musicVolume;
+    }
+}
